42f741597add8e927eb7b25c95679a567e4f30e4
pablozafra90 <pablozafra90@hotmail.com>
2024-05-14 18:41:32 +0200

    197-wip-01

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 
 export const HeroCard = ({ id, superhero, publisher, alter_ego, first_appearance, characters }) => {
 
@@ -15,8 +16,15 @@ export const HeroCard = ({ id, superhero, publisher, alter_ego, first_appearance
                     <div className="card-body">
                         <h5 className="card-title">{ superhero }</h5>
                         <p className="card-text">{ alter_ego}</p>
+                        {
+                            ( alter_ego !== characters ) && ( <p>{ characters }</p> )
+                        }                        
+                        <p className="card-text"> <small className="text-muted">{ first_appearance }</small></p>
 
-                        <p>{ characters }</p>
+                        <Link to={`/hero/${ id }`}>
+                            Más...
+                        </Link>
+                        
                     </div>
                 </div>
             </div>
