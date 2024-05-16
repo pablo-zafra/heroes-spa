1f1d94409c3cc19af2d347439592b6a4e183c926
pablozafra90 <pablozafra90@hotmail.com>
2024-05-16 11:59:49 +0200

    199-completado

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,17 +1,50 @@
-import { Navigate, useParams } from "react-router-dom"
+import { Navigate, useNavigate, useParams } from "react-router-dom"
 import { getHeroById } from "../helpers";
 
 export const HeroPage = () => {
 
-const { heroId, ...rest } = useParams();
+  const { heroId, ...rest } = useParams();
+  const navigate = useNavigate();
 
-const hero = getHeroById( heroId );
+  const hero = getHeroById( heroId );
 
-if ( !hero ) {
-   return <Navigate to="/Marvel" />
-}
+  const onNavigateBack = () => {
+    navigate(-1);
+  }
 
-  return (
-    <h1>HeroPage</h1>
-  )
+  if ( !hero ) {
+    return <Navigate to="/Marvel" />
+  }
+
+    return (
+      <div className="row mt-5">
+        <div className="col-4">
+          <img
+            src={`/assets/heroes/${ heroId }.jpg`}
+            alt={ hero.superhero }
+            className="img-thumbnail"
+            />
+        </div>
+
+        <div className="col-8">
+          <h3>{ hero.superhero }</h3>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item"> <b>Alter ego:</b> { hero.alter_ego }</li>
+            <li className="list-group-item"> <b>Publisher:</b> { hero.publisher }</li>
+            <li className="list-group-item"> <b>First appearance:</b> { hero.first_appearance }</li>
+
+            <h5 className="mt-3"> Characters: </h5>
+            <p>{ hero.characters }</p>
+
+            <button
+              className="btn btn-outline-primary"
+              onClick={ onNavigateBack }
+            >
+                Back
+            </button>
+
+          </ul>
+        </div>
+      </div>
+    )
 }
